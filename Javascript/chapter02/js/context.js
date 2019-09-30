c5b95a6dc35f48b88c08886d352713bbfc7e4574
Vinije <vaclav.pfudl@gmail.com>
2019-09-30 19:31:20 +0200

    Zakldani osvetleni

--- Javascript/chapter02/js/context.js
+++ Javascript/chapter02/js/context.js
@@ -4,7 +4,10 @@ let gl, program, squareVAO, squareIndexBuffer, indices;
 let model, coneVAO, 
 modelMatrix = mat4.create(),
 projMatrix = mat4.create(),
-parts = [];
+parts = [],
+lightDiffuseColor = [1, 1, 1],
+lightDirection = [0, -1, -1],
+normalMatrix = mat4.create();
 
 
 function updateClearColor(...color){
@@ -91,10 +94,16 @@ function initProgram(){
     // Use this program instance
     gl.useProgram(program);
 
+    // Set locations onto the `program` instance
+    program.aVertexPosition = gl.getAttribLocation(program, 'aVertexPosition');
+    program.aVertexNormal = gl.getAttribLocation(program, 'aVertexNormal');
+
     program.uProjMatrix = gl.getUniformLocation(program, 'uProjMatrix');
     program.uMVMatrix = gl.getUniformLocation(program, 'uMVMatrix');
-    program.aVertexPosition = gl.getAttribLocation(program, 'aVertexPosition');
     program.uModelColor = gl.getUniformLocation(program, 'uModelColor');
+    program.uNormalMatrix = gl.getUniformLocation(program, 'uNormalMatrix');
+    program.uLightDirection = gl.getUniformLocation(program, 'uLightDirection');
+    program.uLightDiffuse = gl.getUniformLocation(program, 'uLightDiffuse');
 
 }
 
@@ -151,7 +160,7 @@ function loadMoreParts(filePath){
           // Bind VAO
           gl.bindVertexArray(vao);
 
-          // VBO
+          // VBO vertices
           const vertexBufferObject = gl.createBuffer();
           gl.bindBuffer(gl.ARRAY_BUFFER, vertexBufferObject);
           gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data.vertices), gl.STATIC_DRAW);
@@ -260,8 +269,14 @@ function drawParts(){
     mat4.rotate(modelMatrix, modelMatrix, 30 * Math.PI / 180, [1, 0, 0]);
     mat4.rotate(modelMatrix, modelMatrix, 30 * Math.PI / 180, [0, 1, 0]);
 
+
+    mat4.copy(normalMatrix, modelMatrix);
+    mat4.invert(normalMatrix, normalMatrix);
+    mat4.transpose(normalMatrix, normalMatrix);
+
     gl.uniformMatrix4fv(program.uProjMatrix, false, projMatrix);
     gl.uniformMatrix4fv(program.uMVMatrix, false, modelMatrix);
+    gl.uniformMatrix4fv(program.uNormalMatrix, false, normalMatrix);
 
     // Iterate over every part inside of the `parts` array
     parts.forEach(part => {
@@ -284,6 +299,11 @@ function render(){
     drawParts();
 }
 
+function initLights(){
+    gl.uniform3fv(program.uLightDirection, lightDirection);
+    gl.uniform3fv(program.uLightDiffuse, lightDiffuseColor);
+}
+
 function init()
 {
     const canvas = utils.getCanvas('webgl-canvas');
@@ -301,10 +321,10 @@ function init()
 
     initProgram();
     loadMoreParts('common/models/nissan-gtr');
+    initNormals();
+    initLights();
     render();
     
-    
-
     window.onkeydown = checkKey;
 }
 
