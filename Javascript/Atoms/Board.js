8d648a5b22d85af1febbd2cdaaf2cf4bb1aa4d59
Václav Pfudl <vaclav.pfudl@gmail.com>
2019-11-08 13:19:00 +0100

    Dokonceni webgl knihy + pocatek aotmu

---
+++ Javascript/Atoms/Board.js
@@ -0,0 +1,11 @@
+var Board = [];
+
+var N = 6;
+
+//dvourozmerne pole vyplnene nulami 
+for (let r = 0; r < N; r++) {
+    Board.push([]);
+    for (let c = 0; c < N; c++) {
+        Board[i].push(0);
+    }  
+}
